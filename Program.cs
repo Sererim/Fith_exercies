f484a7f58ebbb444d5c738b976336a3b895a2163
Sererim <danielleymanw@gmail.com>
2022-10-04 18:27:06 +0300

    Get matrix method and show method are added.

--- Program.cs
+++ Program.cs
@@ -3,8 +3,13 @@ int Main()
 {
 
     ushort size = 5;
-    int[] matrix = new int [5] {0, 0, 0, 0, 0};
-    Homework.ShowMatrix(matrix);
+    
+    int[] arr = new int [size];
+
+    arr = Homework.GetMatrix(size);
+
+    Homework.ShowMatrix(arr);
+
     Console.WriteLine();
     return 0;
 
@@ -14,6 +19,20 @@ Main();
 
 class Homework{
 
+    // Method for an array generation, because there are multiple arrays to be generated.
+    // Still main will have only one array.
+    public static int[] GetMatrix(ushort size)
+    {
+        int[] matrix = new int [size];
+        Random rand = new Random();
+
+        for(int i = 0; i < matrix.Length; i++)
+            matrix[i] = rand.Next(-1000, 1000);
+
+        return matrix;
+    }
+
+    // Method for showing an array.
     public static void ShowMatrix(int[] matrix)
     {
         for(int i = 0; i < matrix.Length; i++)
