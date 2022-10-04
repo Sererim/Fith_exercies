bf0a31e457f7ab3d2f2412ec6fef46cb78d7e67a
Sererim <danielleymanw@gmail.com>
2022-10-04 18:09:50 +0300

    Initial innit.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+int Main()
+{
+
+    ushort size = 5;
+    int[] matrix = new int [5] {0, 0, 0, 0, 0};
+    Homework.ShowMatrix(matrix);
+    Console.WriteLine();
+    return 0;
+
+}
+
+Main();
+
+class Homework{
+
+    public static void ShowMatrix(int[] matrix)
+    {
+        for(int i = 0; i < matrix.Length; i++)
+            Console.Write($"|{matrix[i]}| ");
+        Console.WriteLine();
+    }
+
+
+}
